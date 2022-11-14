78b3d1061f91034fd660e1ebca7963e9e69fa69b
kewen84121 <35323665@qq.com>
2022-11-15 05:02:10 +0800

    "new basic version"

--- src/App.js
+++ src/App.js
@@ -1,45 +1,79 @@
-import { useState, createContext } from 'react'
-import ShowInfo from './components/ShowInfo'
-import HistoryList from './components/HistoryList'
+import { useState, useRef } from 'react'
 import './app.scss'
 
-export const Context = createContext()
-let arr = []
-
 function App() {
-  const [amount, setAmount] = useState('')
-  const [percent, setPercent] = useState('')
-  const [tipVal, setTipVal] = useState('')
+  const [amount, setAmount] = useState(0)
+  const [percent, setPercent] = useState(0)
+  const [tip, setTip] = useState(0)
+  const [items, setItems] = useState([])
+
+  let amountRef = useRef(amount)
+  let percentRef = useRef(percent)
+  let tipRef = useRef(tip)
+
+  const amountHandle = (e) => {
+    setAmount(+e.target.value)
+    amountRef.current = +e.target.value
+  }
+
+  const percentHandle = (e) => {
+    setPercent(+e.target.value)
+    percentRef.current = +e.target.value
+  }
 
   const calc = () => {
-    let a = +amount
-    let p = +percent
-    let res = (a * p) / 100
-    setTipVal(res.toString())
-    arr.unshift({ a: amount, p: percent, res })
+    let res = amount * percent / 100
+    setTip(res)
+    tipRef.current = res
+    setItems([
+      ...items,
+      {
+        amount: amountRef.current,
+        percent: percentRef.current,
+        tip: tipRef.current
+      }
+    ])
+    setAmount(0)
+    setPercent(0)
+  }
+
+  const deleteItem = (index) => {
+    setItems(prev => {
+      return prev.filter((_, idx) => idx !== index)
+    })
   }
 
   return (
-    <div className="App" >
+    < div className="App" >
       <div className='appHeader'>
-        <Context.Provider value={{ amount, percent, tipVal, arr }}>
-          <header className='tipHeader'>Tip Calculator</header>
-          <main>
-            <div className='amountDiv'>
-              Amount($) <input type='text' value={amount} onChange={(e) => setAmount(e.target.value)} />
-            </div>
-            <div className='percentDiv'>
-              Percentage(%) <input type='text' value={percent} onChange={(e) => setPercent(e.target.value)} />
-            </div>
-            <button className='handleBtn' onClick={() => calc()}>
-              Calculate
-            </button>
-            <ShowInfo />
-            <HistoryList />
-          </main>
-        </Context.Provider>
+        <header className='tipHeader'>Tip Calculator</header>
+        <main>
+          <div className='amountDiv'>
+            Amount($) <input type='text' value={amount} onChange={amountHandle} />
+          </div>
+          <div className='percentDiv'>
+            Percentage(%) <input type='text' value={percent} onChange={percentHandle} />
+          </div>
+          <button className='handleBtn' onClick={calc}>
+            Calculate
+          </button>
+          <div className="showInfoItem">
+            Amount: ${amount}, Percentage: {percent}%, Tip: ${tip}
+          </div>
+          <header>History</header>
+          <div className='historyList'>
+            {
+              items.map((item, index) => (
+                <div className="historyItem" key={index}>
+                  Amount: ${item.amount}, Percentage: {item.percent}%, Tip: ${item.tip}
+                  <span onClick={() => deleteItem(index)}>X</span>
+                </div>
+              ))
+            }
+          </div>
+        </main>
       </div>
-    </div>
+    </div >
   )
 }
 
